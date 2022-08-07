9b3ca8b49a34c3ca93cb8333c15b571a9965ebdc
Thiago <thiago.pereira@houseasy.net>
2022-08-07 19:51:49 -0300

    🎉 Função de atualizar usuários

--- 24 - CRUD básico/servidor/servidor.go
+++ 24 - CRUD básico/servidor/servidor.go
@@ -141,4 +141,47 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {}
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+		return
+	}
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Erro ao ler o corpo da requisição"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+	// update, dele e post usamos statement, operações que não são de consultas
+
+	defer db.Close()
+
+	statement, erro := db.Prepare("update usuarios set nome = ?, email = ? where id = ?")
+
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
